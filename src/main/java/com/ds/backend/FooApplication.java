3208a2a5199ac7a4cd20bc79684eaabeaf9d99f2
rochana <rochana.atapattu2@gmail.com>
2018-05-24 12:03:53 +0530

    empty

---
+++ src/main/java/com/ds/backend/FooApplication.java
@@ -0,0 +1,12 @@
+package com.ds.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FooApplication.class, args);
+	}
+}
